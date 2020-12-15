cae3b46792c2a7f4ea44b00daf31ccdd03a5015f
Саня <sanya@Sergejs-MacBook-Air.local>
2020-12-15 14:35:39 +0600

    First creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reiteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
